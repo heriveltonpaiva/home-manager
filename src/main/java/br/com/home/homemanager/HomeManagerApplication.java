2e72324ce101560bd74b43ec46be9b1e218eb7ff
hpaiva <hpaiva@uolinc.com>
2019-02-10 14:58:03 -0200

    Initial commit

---
+++ src/main/java/br/com/home/homemanager/HomeManagerApplication.java
@@ -0,0 +1,14 @@
+package br.com.home.homemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeManagerApplication.class, args);
+	}
+
+}
+
